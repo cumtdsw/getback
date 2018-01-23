5ded17c0ad0be963a0dcdaa4c3b230b64a4b3a7f
cumtdsw <cumt_dsw@163.com>
2018-01-23 16:39:02 +0300

    XML utils

---
+++ src/main/java/com/dsw/getback/util/XmlUtil.java
@@ -0,0 +1,36 @@
+package com.dsw.getback.util;
+
+import java.io.File;
+import java.io.IOException;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.w3c.dom.Document;
+import org.xml.sax.SAXException;
+
+public class XmlUtil {
+
+	private static Logger logger = LogManager.getLogger(XmlUtil.class);
+
+	DocumentBuilderFactory builderFactory = DocumentBuilderFactory.newInstance();
+
+	public Document parse(String filePath) {
+		logger.info("filePath: " + filePath);
+		Document document = null;
+		try {
+			DocumentBuilder builder = builderFactory.newDocumentBuilder();
+			document = builder.parse(new File(filePath));
+		} catch (ParserConfigurationException e) {
+			logger.error(e.getMessage(), e);
+		} catch (SAXException e) {
+			logger.error(e.getMessage(), e);
+		} catch (IOException e) {
+			logger.error(e.getMessage(), e);
+		}
+		return document;
+	}
+}
